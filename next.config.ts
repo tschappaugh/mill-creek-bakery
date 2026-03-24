76f4d11fd760f1590b8b228c332dcfe71e2e5696
Tony Schappaugh <tschappaugh@page2studios.com>
2026-03-24 10:44:35 -0500

    Fix: Update Next Config to allow for images served by CDN

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'tonyschappaugh.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+      },
     ],
   },
 }
